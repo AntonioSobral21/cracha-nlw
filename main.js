c3e8b4af33dd131b215f446a98161230028742a0
Tonny Sobral <92737669+AntonioSobral21@users.noreply.github.com>
2021-10-23 00:13:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const linksSocialMedia = {
+  github: 'antoniosobral21',
+  instagram: 'tonny_sobral',
+  linkedin: 'in/antônio-sobral-7414b4224/'
+}
+
+function changeSociaLMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSociaLMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
